8f1aa3e76d881c625b947da8df32d472c1b8c547
mandeep <singh.mr.1996@gmail.com>
2021-06-05 10:33:18 +0530

    uc2_Simulator_Loop_Through_Multipul_Time_Of_Flipping_Coin_Store_The_Singlet_Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 echo "FLIP COIN SIMULATION PUSH ON MASTER"
 HEAD=0
+headCount=0
+tailCount=0
+declare -A singletFlip
+read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
+for(( count=0; count<$numberOfCoinFlip; count++ ))
+do
+   FlipCoin=$(( RANDOM%2 ))
 
-flipCoin=$((RANDOM%2))
-if [ $flipCoin == $HEAD ]
-then
-	echo "Head"
-else
-	echo "Tail"
+   if [ $FlipCoin -eq $HEAD ]
+   then
+      singletFlip[HEAD]=$((++headCount))
+   else
+      singletFlip[TAIL]=$((++tailCount))
+   fi
+done
+
+singletHeadPercentage=$(( (headCount*100)/numberOfCoinFlip ))
+singletTailPercentage=$(( tailCount*100)/numberOfCoinFlip ))
+
+echo "To single head percentage and head count:$headCount : " $singletHeadPercentage
+echo "To single tail percentage and tail count:$tailCount : " $singletTailPercentage
